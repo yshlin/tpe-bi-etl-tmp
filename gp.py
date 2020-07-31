7b806dc0e1d3bf3c78500ebccc898c9a0bbfaf9e
Eddie Lin <yshlin@gmail.com>
2020-07-31 08:54:37 +0800

    Fix file names for utm breakdown.

--- gp.py
+++ gp.py
@@ -14,12 +14,12 @@ def extract_load_gcs(src, prefix, dst):
         os.system('gsutil cp %s%s%s_country.csv ./gp/' % (src, prefix, mo))
         os.system('gsutil cp %s%s%s_utm_tagged.csv ./gp/' % (src, prefix, mo))
         os.system('iconv -f utf-16 -t utf-8 ./gp/%s%s_country.csv > ./gp/%s%s_country_utf8.csv' % (prefix, mo, prefix, mo))
-        os.system('iconv -f utf-16 -t utf-8 ./gp/%s%s_country.csv > ./gp/%s%s_utm_tagged_utf8.csv' % (prefix, mo, prefix, mo))
+        os.system('iconv -f utf-16 -t utf-8 ./gp/%s%s_utm_tagged.csv > ./gp/%s%s_utm_tagged_utf8.csv' % (prefix, mo, prefix, mo))
         os.system('file -I ./gp/%s%s_country_utf8.csv' % (prefix, mo))
         os.system('file -I ./gp/%s%s_utm_tagged_utf8.csv' % (prefix, mo))
         os.system('gsutil cp ./gp/%s%s_country_utf8.csv %s' % (prefix, mo, dst))
         os.system('gsutil cp ./gp/%s%s_utm_tagged_utf8.csv %s' % (prefix, mo, dst))
 
 
-extract_load_gcs(GP_SOURCE, 'retained_installers_org.mozilla.firefox.vpn_', '%s/gp/' % GP_DEST)
-extract_load_gcs(GP_SOURCE, 'retained_installers_org.mozilla.rocket_', '%s/gp/' % GP_DEST)
+extract_load_gcs(GP_SOURCE, 'retained_installers_org.mozilla.firefox.vpn_', '%s/mozvpn/gp/' % GP_DEST)
+extract_load_gcs(GP_SOURCE, 'retained_installers_org.mozilla.rocket_', '%s/mango/gp/' % GP_DEST)
